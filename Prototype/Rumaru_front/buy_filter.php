3959b6cd0f969ce4b04ae9d68d90ae982952aaea
MHabiburRahman7 <george_the@ymail.com>
2017-11-10 14:42:28 +0900

    to reduce difficulties, i put every front end at Prototype folder

---
+++ Prototype/Rumaru_front/buy_filter.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+	<?php include 'head.php';?>
+	
+	<body id="page-top">
+	
+	<?php include 'navbar.php';?>
+
+    <!-- Services -->
+    <section id="services" class="" height="100vh" >
+      <div class="container" style="margin-left:1%; margin-right:0%; padding-top:0%">
+        <div class="row">
+			<div style="width:80%">
+				<img src="img/portfolio/map_legend.png" width="90%">
+			</div>
+			<div style="padding-right:0; margin:0; width:20%">
+				<div class="container">
+					<div class="row">
+						<h2>Somewhere Else</h2>
+						<p>Showing 1201 result</p>
+					</div>
+					<div class="row">
+						<h3>Address A</h3>
+						<img src="img/portfolio/map_legend.png" width="80%" height="80%">
+						<p>Detail</p>
+					</div>
+				</div>
+			</div>
+			<div>
+				<h3>Address A</h3>
+				<img src="img/portfolio/map_legend.png" width="50%">
+				<p>Detail</p>
+			</div>
+        </div>
+      </div>
+    </section>
+	
+	<!-- Team -->
+    <section id="team" class='bg-light'>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">Our Amazing Team</h2>
+            <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="team-member">
+              <img class="mx-auto rounded-circle" src="img/team/1.jpg" alt="">
+              <h4>Kay Garland</h4>
+              <p class="text-muted">Lead Designer</p>
+              <ul class="list-inline social-buttons">
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="team-member">
+              <img class="mx-auto rounded-circle" src="img/team/2.jpg" alt="">
+              <h4>Larry Parker</h4>
+              <p class="text-muted">Lead Marketer</p>
+              <ul class="list-inline social-buttons">
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="team-member">
+              <img class="mx-auto rounded-circle" src="img/team/3.jpg" alt="">
+              <h4>Diana Pertersen</h4>
+              <p class="text-muted">Lead Developer</p>
+              <ul class="list-inline social-buttons">
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+	<?php include 'contact.php';?>
+	<?php include 'footer.php';?>
+	<?php include 'boostrap_closing.php';?>
+	
+	</body>
+
+</html>
---
+++ Prototype/Rumaru_front/index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+	<?php include 'head.php';?>
+	
+	<body id="page-top">
+	
+	<?php include 'navbar.php';?>
+	
+	<!-- Header -->
+    <header class="masthead">
+      <div class="container">
+        <div class="intro-text">
+          <div class="intro-lead-in">Sell your home for best price</div>
+          <!--
+		  <div class="intro-heading text-uppercase">Find your dream house</div>
+		  -->
+			<form style="text-align:center">
+			  <div class="form-group">
+				<input type='text' class='form-control' style="min-width:70vh" name='address' placeholder='City, Adress, School, Agent, ZIP'>
+			  </div>
+
+			  <button type="submit" class="btn btn-primary">Submit</button>
+			</form>
+			
+		  <br>
+		  <p>
+			Sell your house with good price
+			<br>
+			Whenever you ready with your house, just contact our agent in order to consult with us
+		  </p>
+		  <br>
+          <a class="btn btn-primary text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
+        </div>
+      </div>
+    </header>
+
+    <!-- Services -->
+    <section id="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">Loyal agent, reliable consultant</h2>
+            <h3 class="section-subheading text-muted">We're reliable, loyal, agent who get to know your houses<br>and we use online tools to make you smarter and faster</h3>
+			<a class="btn btn-primary text-uppercase js-scroll-trigger" href="#services">See Our Story</a>
+		  </div>
+        </div>
+      </div>
+    </section>
+	
+	<!-- Team -->
+    <section id="team" class='bg-light'>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">Our Amazing Team</h2>
+            <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="team-member">
+              <img class="mx-auto rounded-circle" src="img/team/1.jpg" alt="">
+              <h4>Kay Garland</h4>
+              <p class="text-muted">Lead Designer</p>
+              <ul class="list-inline social-buttons">
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="team-member">
+              <img class="mx-auto rounded-circle" src="img/team/2.jpg" alt="">
+              <h4>Larry Parker</h4>
+              <p class="text-muted">Lead Marketer</p>
+              <ul class="list-inline social-buttons">
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="team-member">
+              <img class="mx-auto rounded-circle" src="img/team/3.jpg" alt="">
+              <h4>Diana Pertersen</h4>
+              <p class="text-muted">Lead Developer</p>
+              <ul class="list-inline social-buttons">
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+	<?php include 'contact.php';?>
+	<?php include 'footer.php';?>
+	<?php include 'boostrap_closing.php';?>
+	
+	</body>
+
+</html>
